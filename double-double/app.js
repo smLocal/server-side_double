2b185872481b5099bc9d6f9d8cd9f0b7b86715da
Carlos Velasquez <cv.velasquez@gmail.com>
2016-05-11 16:40:22 -0700

    phil magic post

--- double-double/app.js
+++ double-double/app.js
@@ -12,7 +12,6 @@ var db = require('./model/todo.js');
 
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -28,8 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api/todos', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
