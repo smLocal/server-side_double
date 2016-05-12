1df59c8639c3d8aec31b5a68aa8cbbd261a15b5f
david szyszkowski <try2murkme@gmail.com>
2016-05-11 17:50:17 -0700

    added some matt code

--- double-double/app.js
+++ double-double/app.js
@@ -50,6 +50,14 @@ if (app.get('env') === 'development') {
   });
 }
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+app.use(allowCrossDomain);
+
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
